bbc12fda851024ed0cd69a8c8fadacf6f9d593cd
taichi221228 <taichi221228@icloud.com>
2024-04-17 22:31:10 +0900

    chore: Add ESLint import rules file and update config

---
+++ .eslintrc.import.cjs
@@ -0,0 +1,61 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+	extends: ["plugin:import/recommended"],
+	plugins: ["import"],
+	rules: {
+		"import/order": [
+			"error",
+			{
+				alphabetize: {
+					order: "asc",
+				},
+				groups: [
+					"builtin",
+					"external",
+					"internal",
+					["sibling", "parent"],
+					"index",
+					"object",
+					"type",
+				],
+				"newlines-between": "always",
+				pathGroups: [
+					{
+						group: "builtin",
+						pattern: "@builder.io/qwik?(-city)**",
+					},
+					{
+						group: "external",
+						pattern: "@qwik?(-city)**",
+					},
+					{
+						group: "internal",
+						pattern: "~/constants",
+					},
+					{
+						group: "internal",
+						pattern: "~/components",
+					},
+					{
+						group: "internal",
+						pattern: "~/hooks",
+					},
+					{
+						group: "sibling",
+						pattern: "./**",
+					},
+					{
+						group: "parent",
+						pattern: "../**",
+					},
+				],
+				pathGroupsExcludedImportTypes: [],
+			},
+		],
+	},
+	settings: {
+		"import/resolver": {
+			typescript: true,
+		},
+	},
+};
